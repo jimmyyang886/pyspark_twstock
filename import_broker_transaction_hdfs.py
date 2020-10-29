783a869976d3f4a4e94d1cae32394b6c5d90e001
JimmyYang <jimmyyang886@gmail.com>
2020-10-29 09:34:34 +0800

    Add files via upload

---
+++ import_broker_transaction_hdfs.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from pyspark import SparkConf, SparkContext, sql
+from pyspark.sql import SparkSession
+from datetime import datetime
+#from pyspark.sql.types import StringType,DateType, FloatType, IntegerType, DecimalType
+
+#DataTypeList
+'''
+'BinaryType', 'BooleanType', 'ByteType', 'DateType',
+'DecimalType', 'DoubleType', 'FloatType', 'IntegerType',
+LongType', 'ShortType', 'StringType', 'TimestampType'
+'''
+## Import data from mysql to hdfs
+# Changing path, IP,  user_name, user_password for your system
+
+        
+def broker_transaction2parquet(date):
+    spark = SparkSession.builder\
+                        .appName("Import_broker_transaction")\
+                        .getOrCreate()
+
+
+    schema = "bID STRING, sID STRING, Date Date, sn INTEGER, price FLOAT, buy INTEGER, sell INTEGER"
+    
+    broker_transaction = spark.read.jdbc(
+                                         url = "jdbc:mysql://192.168.246.128:3306/twstock",
+                                         table = f"(SELECT * FROM broker_transaction WHERE Date = '{date}')AS my_table",
+                                         properties = {'user': uname, 'password': passwd, 'customSchema' :schema}
+                                        )
+    #broker_transaction.printSchema()
+    #broker_transaction.show(5)
+
+    path = "hdfs://master/user/spark/twstock/raw_data/broker_transaction"
+    return broker_transaction.write.mode('overwrite').parquet(f"{path}/{date}.parquet")
+    
+
+if __name__=='__main__':
+
+    uname = 'xxxx'
+    passwd = 'xxxx'
+
+    #date = '2020-05-22'
+    #broker_transaction2parquet(date)
+
+    spark = SparkSession.builder\
+                        .appName("Get_Transactions_Date")\
+                        .getOrCreate()
+
+    dateDF = spark.read.jdbc(url = "jdbc:mysql://192.168.246.128:3306/twstock",
+                              table = f"(SELECT distinct Date FROM broker_transaction)AS my_table",
+                              properties = {'user': uname, 'password': passwd})
+
+
+    dateRDD=dateDF.rdd.map(lambda x: x[0].isoformat())
+    dateRDD.map(broker_transaction2parquet).collect() 
+
